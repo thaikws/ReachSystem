4e87a03428752fe2623c3ba421491c0598bee4bb
malu <maria.silva5315@etec.sp.gov.br>
2026-03-27 17:16:32 -0300

    model Consulta

---
+++ ReachSystem/Models/Consulta.cs
@@ -0,0 +1,11 @@
+﻿namespace ReachSystem.Models
+{
+    public class Consulta
+    {
+        public int ConsultaID { get; set; }
+        public int AnimalId { get; set; }
+        public animal animal { get; set; }
+        public string Data { get; set; }
+        public string Descricao { get; set; }
+    }
+}
