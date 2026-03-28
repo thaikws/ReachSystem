a186d0c76b89527d7e80c9d0623edd9e03cc57a8
Thaina <l.rodrigues.lima@gmail.com>
2026-03-28 02:12:08 -0300

    feat(data): definição dos relacionamentos das tabelas Animais e FichaSaude

--- ReachSystem/Data/ApplicationDbContext.cs
+++ ReachSystem/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ReachSystem.Models;
+
 
 namespace ReachSystem.Data
 {
@@ -8,6 +10,19 @@ namespace ReachSystem.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<Animal> Animais { get; set; }
+        public DbSet<FichaSaude> FichasSaude { get; set; }
+
+        //Configuração do relacionamento um-para-um entre Animal e FichaSaude
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Animal>()
+                .HasOne(a => a.FichaSaude)
+                .WithOne(f => f.Animal)
+                .HasForeignKey<FichaSaude>(f => f.AnimalId);
         }
     }
 }
--- ReachSystem/Models/Animal.cs
+++ ReachSystem/Models/Animal.cs
@@ -8,10 +8,24 @@ namespace ReachSystem.Models
         public string Nome { get; set; } = string.Empty;
         public string Especie { get; set; } = string.Empty;
         public int Idade { get; set; }
+        public string Raca { get; set; } = string.Empty;
+        public string Porte { get; set; } = string.Empty;
         public Sexo SexoAnimal { get; set; }
         public Status StatusAnimal { get; set; }
-
         public DateTime DataDeEntrada { get; set; }
         public FichaSaude? FichaSaude { get; set; }
+
+        public Animal(string nome, string especie, int idade, string raca, string porte, Sexo sexo, Status status, DateTime dataDeEntrada, FichaSaude fichaSaude)
+        {
+            Nome = nome;
+            Especie = especie;
+            Idade = idade;
+            Raca = raca;
+            Porte = porte;
+            SexoAnimal = sexo;
+            StatusAnimal = status;
+            DataDeEntrada = dataDeEntrada;
+            FichaSaude = fichaSaude;
+        }
     }
 }
