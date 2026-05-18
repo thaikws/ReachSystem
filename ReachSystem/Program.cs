d8586b3a29a3a076ec4131908471efc3528c5648
Thaina <l.rodrigues.lima@gmail.com>
2026-05-17 23:13:16 -0300

    feat(data): criar admin

--- ReachSystem/Program.cs
+++ ReachSystem/Program.cs
@@ -56,17 +56,8 @@ app.MapRazorPages();
 
 using (var scope = app.Services.CreateScope())
 {
-    var roleManager = scope.ServiceProvider
-        .GetRequiredService<RoleManager<IdentityRole>>();
+    var services = scope.ServiceProvider;
 
-    string[] roles = { "Admin", "Funcionario", "Voluntario" };
-
-    foreach (var role in roles)
-    {
-        if (!await roleManager.RoleExistsAsync(role))
-        {
-            await roleManager.CreateAsync(new IdentityRole(role));
-        }
-    }
+    await IdentitySeeder.SeedRolesAndAdminAsync(services);
 }
 app.Run();
