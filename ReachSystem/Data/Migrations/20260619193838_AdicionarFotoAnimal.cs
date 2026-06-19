a00cc01f3c652cc95ae99e943007c8565aa1e06b
Samuel Amate <amatesamuel1@gmail.com>
2026-06-19 16:39:17 -0300

    feat(data): criar migration fotoAnimal

---
+++ ReachSystem/Data/Migrations/20260619193838_AdicionarFotoAnimal.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReachSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarFotoAnimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Foto",
+                table: "Animais",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Foto",
+                table: "Animais");
+        }
+    }
+}
