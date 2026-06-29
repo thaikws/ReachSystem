9c21bed4074ebd77a34acc70f0c8728f8e54b9c7
Thaina <l.rodrigues.lima@gmail.com>
2026-06-29 20:58:11 -0300

    feat(controller): criar o controller do novo login

---
+++ ReachSystem/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ReachSystem.Models;
+
+namespace ReachSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string email, string senha)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                email,
+                senha,
+                false,
+                false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            ModelState.AddModelError("", "E-mail ou senha inválidos.");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
