7d689cd7a90fbb5192670597fd7de865e815e800
Thaina <l.rodrigues.lima@gmail.com>
2026-06-29 20:46:34 -0300

    feat(controller): criar DashboardController

---
+++ ReachSystem/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReachSystem.Services;
+
+namespace ReachSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly DashboardService _dashboardService;
+
+        public DashboardController(DashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.TotalAnimais = await _dashboardService.GetTotalAnimais();
+            ViewBag.TotalEventos = await _dashboardService.GetTotalEventos();
+            ViewBag.TotalFichas = await _dashboardService.GetTotalFichas();
+            ViewBag.TotalUsuarios = await _dashboardService.GetTotalUsuarios();
+
+            return View();
+        }
+    }
+}
