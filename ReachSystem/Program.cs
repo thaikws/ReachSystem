47f4405dc2413ba3a6f160a10d90caed4847c7b6
Thaina <l.rodrigues.lima@gmail.com>
2026-06-29 21:01:33 -0300

    feat(view): criar página de login nova

--- ReachSystem/Program.cs
+++ ReachSystem/Program.cs
@@ -53,7 +53,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 app.MapRazorPages();
 
 using (var scope = app.Services.CreateScope())
