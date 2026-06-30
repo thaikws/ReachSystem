18eb9d505d638c92498c83da47ca7585287b54b3
Thaina <l.rodrigues.lima@gmail.com>
2026-06-29 21:09:26 -0300

    feat(service): criar service do login

---
+++ ReachSystem/Services/AccountService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ReachSystem.Models;
+
+namespace ReachSystem.Services
+{
+    public class AccountService
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountService(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<SignInResult> LoginAsync(string email, string senha)
+        {
+            return await _signInManager.PasswordSignInAsync(
+                email,
+                senha,
+                isPersistent: false,
+                lockoutOnFailure: false);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
+}
